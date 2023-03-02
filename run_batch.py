5e9ad9558da04593b16d258f3080efe8c31ad616
evans <evans@gmail.com>
2023-03-02 20:07:15 +0800

    update

--- run_batch.py
+++
@@ -1,7 +0,0 @@
-from config import CHAIN
-from subgraph.scanner import BATCH_SCANNER
-from libs.logger import get_logger
-logger = get_logger(__name__)
-logger.info("work start")
-
-BATCH_SCANNER(CHAIN).start()
--- run_ws.py
+++
@@ -1,7 +0,0 @@
-from config import CHAIN
-from subgraph.scanner import WS_SCANNER
-from libs.logger import get_logger
-logger = get_logger(__name__)
-logger.info("work start")
-
-WS_SCANNER(CHAIN).start()
