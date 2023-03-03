355eb6551b2aa750fb81dfd7aa312830e2dfe4a3
evans <evans@gmail.com>
2023-03-03 14:14:02 +0800

    update

--- subgraph/models/withdraw_event.py
+++ subgraph/models/withdraw_event.py
@@ -3,17 +3,19 @@ from libs.logger import get_logger
 from dataclasses import dataclass, field
 from typing import List
 from eth_abi import decode
+from web3 import Web3
+from libs.utils import hex_to_address
 import strawberry
 
-
 logger = get_logger(__name__)
+
 
 @strawberry.type
 @dataclass
 class WithdrawEventEntity():
     tx_hash: str = ""
     block_number: int = 0
-    block_time: int = 0 
+    block_time: int = 0
     token: str = ""
     account: str = ""
     amount: str = ""
@@ -22,11 +24,11 @@ class WithdrawEventEntity():
 
 
 class WithdrawEvent():
-    
+
     def __init__(self, event):
         self.name = self.__class__.__name__
         self.event = event
-        (amount, expiry, nonce) = decode(["uint256","uint256","uint256"], Web3.toBytes(hexstr=event['data']))
+        (amount, expiry, nonce) = decode(["uint256", "uint256", "uint256"], Web3.toBytes(hexstr=event['data']))
         event['token'] = hex_to_address(event['topic1'])
         event['account'] = hex_to_address(event['topic2'])
         event['amount'] = amount
